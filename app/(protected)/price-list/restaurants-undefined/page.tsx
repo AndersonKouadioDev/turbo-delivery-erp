3178e8d5408d4bb7dcba9c96df0b014a03787432
=ouattaraabraham <=ouattara.abraham2019@gmail.com>
2025-03-14 14:45:22 +0100

    price-list

---
+++ app/(protected)/price-list/restaurants-undefined/page.tsx
@@ -0,0 +1,9 @@
+import {Tabs, Tab, Card, CardBody} from "@heroui/react";
+
+export default function RestaurantUndefined() {
+  return (
+    <div className="flex w-full flex-col">
+      liste des restaurent indefinie
+    </div>
+  );
+}
