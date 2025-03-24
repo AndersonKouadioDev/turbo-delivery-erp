c7be61389f2bb1bb44a4cf19a9a74ea25fb80f30
Anderson Kouadio <andersonkouadio0118@gmail.com>
2025-03-24 16:14:11 +0000

    fusion price list page

--- app/(protected)/price-list/page.tsx
+++ app/(protected)/price-list/page.tsx
@@ -4,10 +4,6 @@ import { RestaurantDefini } from '@/types/price-list';
 import Content from './content';
 import { getRestaurantDefined } from '@/src/price-list/price-list.action';
 
-<<<<<<< HEAD
-export default async function Page() {
-    const initialData: RestaurantDefini[] = await getRestaurantDefined();
-=======
 export const metadata: Metadata = {
     title: "Restaurants ayant des livraisons définies ",
     description: "La liste des restaurants qui sont ont des livraisons définies.",
@@ -18,6 +14,5 @@ export default async function Page() {
     const initialData: RestaurantDefini[] = await getRestaurantDefined();
 
       
->>>>>>> origin/abraham
     return <Content initialData={initialData} />;
 }
