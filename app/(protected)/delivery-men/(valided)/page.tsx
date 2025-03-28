3c30d2f685cd748ea6e60e98d6648e9fd7259573
Anderson Kouadio <andersonkouadio0118@gmail.com>
2025-03-28 16:54:23 +0000

    mise a jour

--- app/(protected)/delivery-men/(valided)/page.tsx
+++ app/(protected)/delivery-men/(valided)/page.tsx
@@ -1,20 +1,16 @@
 import { Metadata } from 'next';
-import { getDeliveryMen, getToutLivreurStatus } from '@/src/actions/delivery-men.actions';
+import { getToutLivreurStatus } from '@/src/actions/delivery-men.actions';
 import { PaginatedResponse } from '@/types';
-import { DeliveryMan, LivreurStatutVM } from '@/types/models';
+import { LivreurStatutVM } from '@/types/models';
 import Content from './content';
 import { allRestaurants } from '@/src/actions/restaurants.actions';
-import { auth } from '@/auth';
 
 export const metadata: Metadata = {
-    title: 'Delivery Men',
+  title: 'Delivery Men',
 };
 
-
 export default async function DeliveryMen() {
-    const toutStatutLivreurs: PaginatedResponse<LivreurStatutVM[]> | null = await getToutLivreurStatus(0, 5);
-    const allRestaurant = await allRestaurants();
-    return (
-        <Content initialData={toutStatutLivreurs} restaurants={allRestaurant} />
-    );
+  const toutStatutLivreurs: PaginatedResponse<LivreurStatutVM[]> | null = await getToutLivreurStatus(0, 10);
+  const allRestaurant = await allRestaurants();
+  return <Content initialData={toutStatutLivreurs} restaurants={allRestaurant} />;
 }
